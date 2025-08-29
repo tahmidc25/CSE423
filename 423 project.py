c1c64759295d5ed52616310e436423258db6458e
Mohammad Tahmid Chowdhury <135363356+tahmidc25@users.noreply.github.com>
2025-08-29 14:33:38 +0600

    Update 423 project.py

--- 423 project.py
+++ 423 project.py
@@ -1,2 +1,20 @@
-423 project 
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import math
+import random 
+
+camera_mode = 1  # 1: behind car, 2: top-down, 3: side view
+fovY = 60
+GRID_SIZE = 800
+
+# Car properties
+car_x = 0
+car_z = 0
+car_angle = 0
+car_speed = 0
+max_speed = 8
+acceleration = 0.5
+friction = 0.9
+turn_speed = 3
 
